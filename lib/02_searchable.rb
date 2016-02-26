ec184021c97b20d9f44883e7ad68eee29edc548d
Chris <nope>
2016-02-26 17:47:53 -0500

    Add #belongs_to and #has_many to Associatable module

--- lib/02_searchable.rb
+++ lib/02_searchable.rb
@@ -1,10 +1,10 @@
 require_relative 'db_connection'
 require_relative '01_sql_object'
+# require 'byebug'
 
 module Searchable
   def where(params)
     where_string = params.keys.map { |key| "#{key} = ?" }.join(" AND ")
-
     results = DBConnection.execute(<<-SQL, *params.values)
       SELECT
         *
--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -1,5 +1,6 @@
 require_relative '02_searchable'
 require 'active_support/inflector'
+require 'byebug'
 
 # Phase IIIa
 class AssocOptions
@@ -10,34 +11,61 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    class_name.constantize
   end
 
   def table_name
-    # ...
+    model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+
+    options = {
+      foreign_key: "#{name.to_s.underscore}_id".to_sym,
+      class_name: name.to_s.camelcase,
+      primary_key: :id
+    }.merge(options)
+
+    options.each { |key, value| send("#{key}=", value) }
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    options = {
+      foreign_key: "#{self_class_name.to_s.underscore}_id".to_sym,
+      class_name: name.to_s.camelcase.singularize,
+      primary_key: :id.to_sym
+    }.merge(options)
+
+    options.each { |key, value| send("#{key}=", value) }
   end
 end
 
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    # ...
+    options = BelongsToOptions.new(name, options)
+
+    define_method(name) do
+      key = send(options.send(:foreign_key))
+      target_class = options.send(:model_class)
+      target_class.send(:where, id: key).first
+    end
+
   end
 
   def has_many(name, options = {})
-    # ...
+    options = HasManyOptions.new(name, self.to_s, options)
+
+    define_method(name) do
+      key = send(options.send(:primary_key))
+      target_class = options.send(:model_class)
+      conditions = { options.foreign_key => key }
+      target_class.send(:where, conditions)
+    end
   end
 
   def assoc_options
@@ -46,5 +74,5 @@ module Associatable
 end
 
 class SQLObject
-  # Mixin Associatable here...
+  extend Associatable
 end
